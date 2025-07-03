20b91f4cd3a483a497c693f4000e8cab4b84da14
Haris hamayun <haris1122hamayun@gmail.com>
2025-07-03 21:00:50 +0500

    Add project files.

---
+++ EMPLOYEE API/Models/Entites/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EMPLOYEE_API.Models.Entites
+{
+    public class Employee
+    {
+        public Guid id {  get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public decimal Salary { get; set; }
+
+
+
+    }
+}
